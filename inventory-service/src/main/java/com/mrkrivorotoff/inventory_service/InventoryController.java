3b7a7335034d19ba9887ae0188bf3ef06c83e8c9
Dmitrii Petrov <mrkrivorotoff@gmail.com>
2026-09-05 19:43:13 +0300

    Add logging

--- inventory-service/src/main/java/com/mrkrivorotoff/inventory_service/InventoryController.java
+++ inventory-service/src/main/java/com/mrkrivorotoff/inventory_service/InventoryController.java
@@ -1,5 +1,7 @@
 package com.mrkrivorotoff.inventory_service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +12,12 @@ import java.util.Map;
 @Controller
 @RequestMapping("inventory")
 public final class InventoryController {
+    private static final Logger log = LoggerFactory.getLogger(InventoryController.class);
+
     @ResponseBody
     @GetMapping("user_currencies")
     public Map<String, Long> getUserCurrencies() {
+        log.info("inventory.getUserCurrencies requested");
         return Map.of("Gold", 1000L);
     }
 }
--- login-service/src/main/java/com/mrkrivorotoff/login_service/LoginController.java
+++ login-service/src/main/java/com/mrkrivorotoff/login_service/LoginController.java
@@ -1,5 +1,7 @@
 package com.mrkrivorotoff.login_service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,9 +10,12 @@ import java.util.UUID;
 
 @Controller
 public final class LoginController {
+    private static final Logger log = LoggerFactory.getLogger(LoginController.class);
+
     @ResponseBody
     @PostMapping("login")
     public String login() {
+        log.info("login requested");
         return UUID.randomUUID().toString();
     }
 }
