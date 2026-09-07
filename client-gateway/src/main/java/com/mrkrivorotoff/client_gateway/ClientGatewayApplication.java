77ee52451079e5c3c234a00c9fd26d2d72f14ade
Dmitrii Petrov <mrkrivorotoff@gmail.com>
2026-09-07 23:50:22 +0300

    Add API gateway for service routing

---
+++ client-gateway/src/main/java/com/mrkrivorotoff/client_gateway/ClientGatewayApplication.java
@@ -0,0 +1,11 @@
+package com.mrkrivorotoff.client_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public final class ClientGatewayApplication {
+    static void main(String[] args) {
+        SpringApplication.run(ClientGatewayApplication.class, args);
+    }
+}
